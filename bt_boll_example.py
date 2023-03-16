a30358777fecca605a461d33023fbaf30567487c
jxliaocb <dennisliao.everest@gmail.com>
2023-03-16 12:31:19 +1100

    ADD more Data csv

--- bt_boll_example.py
+++ bt_boll_example.py
@@ -13,7 +13,7 @@ class BBStrategy(bt.Strategy):
     )
     def __init__(self):
         self.bollinger = bt.indicators.BollingerBands(period=self.params.window, devfactor=self.params.std)
-        
+
     def next(self):
         if self.data.close < self.bollinger.lines.bot:
             self.buy()
@@ -26,13 +26,15 @@ symbol = 'A2M.AX'
 # data = bt.feeds.PandasData(dataname=yf.download(symbol, start=start_date, end=end_date))
 
 # Data from local csv file from yahoo finance history
-datapath = ('./yf/A2M.AX.csv')   
-dataframe = pd.read_csv(datapath, index_col=0, parse_dates=True)                            
-dataframe['openinterest'] = 0    
-data = bt.feeds.PandasData(dataname=dataframe) 
+datapath = ('./yf/A2M.AX.csv')
+dataframe = pd.read_csv(datapath, index_col=0, parse_dates=True)
+dataframe['openinterest'] = 0
+data = bt.feeds.PandasData(dataname=dataframe)
 
 # Create cerebro instance
 cerebro = bt.Cerebro()
+
+cerebro.broker.set_cash(100000)
 
 # Add data feed
 cerebro.adddata(data)
@@ -45,6 +47,8 @@ cerebro.addanalyzer(bt.analyzers.Returns, _name='returns')
 
 # Run backtest
 results = cerebro.run()
+
+print(cerebro.broker.get_cash())
 
 # Print results
 # print(f"Cumulative Returns: {results[0].analyzers.returns.get_analysis()['cumulative']}")
--- read_csv.py
+++ read_csv.py
@@ -1,10 +1,19 @@
 import datetime
 import backtrader as bt
 import backtrader.feeds as btfeed
+import yfinance as yf
 
+
+# Download data from Yahoo Finance
+# symbol = 'AAPL'
+# start_date = '2015-01-01'
+# end_date = '2023-03-04'
+# dataname = yf.download(symbol, start=start_date, end=end_date)
+# data = bt.feeds.PandasData(dataname)
+# data.to_csv('./yf/AAPL.csv')
 
 data = btfeed.GenericCSVData(
-    dataname='./datas_yf/A2M.AX.csv',
+    dataname='./yf/A2M.AX.csv',
 
 )
-print(data.lines)
+print(data)
